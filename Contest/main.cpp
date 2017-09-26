b26e0d363a2e785eff3c2ef2a73a78cf61933d7d
Islam <islam7450@gmail.com>
2017-09-26 23:08:41 +0300

    Initial Commit

---
+++ Contest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Contest
+//
+//  Created by Ислам on 26.09.2017.
+//  Copyright © 2017 Ислам. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
